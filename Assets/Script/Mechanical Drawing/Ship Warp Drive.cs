6c014fb8d1c76373af7e50545e657d29764400f9
David Searle <searled@shernet.sheridancollege.ca>
2025-01-31 17:54:29 -0500

    VICTORY.  MANAGED TO GET THE SHIP TO GO BACK AND FORTH RAHHHHG

--- Assets/Script/Mechanical Drawing/Ship Warp Drive.cs
+++ Assets/Script/Mechanical Drawing/Ship Warp Drive.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class ShipWarpDrive : MonoBehaviour
 {
 // set a float value for the speed of the ship entering warp speed   
-float ftlDriveSpeed = 2f;
+float ftlDriveSpeed = 1f;
 //have a boolean value for the warp drive being on and off    
 bool warpDriveOn = false;
+//have another bool for direction
+bool exitWarp = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,15 +22,38 @@ bool warpDriveOn = false;
         Vector3 pos = transform.position;
     if (Input.GetKeyDown(KeyCode.Space))
     {
-    warpDriveOn = true;
+         
+    warpDriveOn = !warpDriveOn;
+    
     }
+  
     if (warpDriveOn == true)
     {
+        ftlDriveSpeed = 2;
         pos.x += ftlDriveSpeed;
        transform.position = pos;
         if(pos.x >= 0) {
             ftlDriveSpeed = 0;
+            warpDriveOn = false;
         }
     }
-}
+   
+    if (pos.x >= 0 && Input.GetKeyDown(KeyCode.Space) ) {
+   exitWarp = true;
+   }
+   if (exitWarp == true) {
+   ftlDriveSpeed = 1f;
+        pos.x -= ftlDriveSpeed;
+        transform.position = pos;
+    
+        if(pos.x < -5.85) {
+          ftlDriveSpeed = 0;
+          exitWarp = false;
+        }
+    }
+    
+    
+    
+    
+    }
 }
